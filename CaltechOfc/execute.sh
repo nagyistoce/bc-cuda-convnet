1c90defd845e680b70d16b8b9cb080cac5ff36a4
mimunzar@gmail.com <mimunzar@gmail.com@96411021-4009-aa1f-e783-21546d3fb7df>
2013-02-12 10:57:16 +0000

    new provider

---
+++ CaltechOfc/execute.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Starts training neural net
+
+cifar(){
+python convnet.py   --data-path=./Datasets/CIFAR \
+                    --save-path=./STORAGE/CIFAR \
+                    --test-range=2 \
+                    --train-range=1 \
+                    --layer-def=./example-layers/layers-conv-local-13pct.cfg \
+                    --layer-params=./example-layers/layer-params-conv-local-13pct.cfg \
+                    --data-provider=cifar \
+                    --test-freq=13 \
+                    --crop-border=4 \
+                    --epochs=100
+}
+
+caltech_test(){
+python convnet.py --data-path=./Datasets/Caltech101 \
+                  --save-path=./STORAGE/CALTECH \
+                  --test-range=5-6  \
+                  --train-range=1-4 \
+                  --layer-def=./example-layers/layers-19pct.cfg \
+                  --layer-params=./example-layers/layer-params-19pct.cfg \
+                  --data-provider=caltech101 \
+                  --test-freq=2 \
+                  --epochs=10
+}
+
+caltech_1(){
+python convnet.py --data-path=./Datasets/Caltech101 \
+                  --save-path=./Storage/Caltech_1 \
+                  --test-range=5-6  \
+                  --train-range=1-4 \
+                  --layer-def=./Datasets/Caltech101/1D.cfg \
+                  --layer-params=./Datasets/Caltech101/1P.cfg \
+                  --data-provider=caltech101 \
+                  --test-freq=15 \
+                  --epochs=10
+}
+
+caltech_2(){
+python convnet.py --data-path=./Datasets/Caltech101 \
+                  --save-path=./Storage/Caltech_2 \
+                  --test-range=1  \
+                  --train-range=0 \
+                  --layer-def=./Datasets/Caltech101/1D.cfg \
+                  --layer-params=./Datasets/Caltech101/1P.cfg \
+                  --data-provider=caltech101 \
+                  --test-freq=15 \
+                  --epochs=100
+}
+
+caltech_2
+
+exit 0
